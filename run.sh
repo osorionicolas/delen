d92977f8caae560a3f8209e3a1a5a06952d941d9
osorionicolas <osorionicolas95@gmail.com>
2020-07-19 17:19:26 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker rm delen
 
 docker pull secretcolossus/delen:latest
 
-docker run -it -d --restart=always -p 3000:3000 -p 5000:5000 --name delen secretcolossus/delen:latest
+docker run -it -d --restart=always -p 3000:3000 -p 5000:5000 -v /home/docker/images/delen/files /usr/local/apps/delen/server/files --name delen secretcolossus/delen:latest

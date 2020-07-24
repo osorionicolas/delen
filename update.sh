ea944b19be208fef016ca5e486fb93c1c22403aa
osorionicolas <osorionicolas95@gmail.com>
2020-07-23 22:18:07 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,12 @@
 git stash
 git pull
 chmod 777 *.sh
-npm install -p ./client
-npm run build --prod -p ./client
-mv client/build server
-npm install -p ./server
+cd client
+npm install
+npm run build --prod
+mv build ../server
+cd ../server
+npm install
+cd ..
 ./build.sh
 ./run.sh

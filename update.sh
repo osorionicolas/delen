83b893cd9dfa6bc22d7fbc0c4ee00e20f141786d
osorionicolas <osorionicolas95@gmail.com>
2020-07-23 22:14:02 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 git stash
 git pull
-npm install -p client
-npm run build --prod -p client
+npm install -p ./client
+npm run build --prod -p ./client
 mv client/build server
-npm install -p server
+npm install -p ./server

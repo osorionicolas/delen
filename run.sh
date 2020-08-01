9411c1136fbf9c8fb907b7336ee6e019692ce5ad
Nicolas Osorio <osorionicolas95@gmail.com>
2020-08-01 05:02:45 -0300

    Se removio la variable de entorno debido a que react solo la procesa mediante el archivo .env

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker rm delen
 
 docker pull secretcolossus/delen:latest
 
-docker run -it -d --restart=always -p 5000:5000 -e REACT_APP_SERVER_IP_ADDRESS=apps.local --name delen secretcolossus/delen:latest
+docker run -it -d --restart=always -p 5000:5000 --name delen secretcolossus/delen:latest
